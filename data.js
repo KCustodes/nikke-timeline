6d0c3ee4d91ea0ddfbec8ea72d7c8770aba06f3b
KCustodes <kvplogames@gmail.com>
2026-02-13 12:01:18 -0800

    Update data.js

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ const ENTRIES = [
         content: "The World was similar to what we know now in real life, but slightly more technologically advanced. <ul><li>Manufactures such as Elysion, Missilis and Tetra existed but no exact detail on how big they were aside from Relics stating them to be enterprises. (Early Relics called them by Elysion Enterprises, Missilis Industries, and Tetra Entertainment).</li><li>Military strength similar to IRL or at least slightly more advanced.</li><li>Einkk stated that she is roughly 20 years old (Memory of Goddess secret cutscene) which puts her origins during Pre-First Invasion but no other specifics on who or what made her.</li><li>Based off Chapter 43, the VTC has existed for a long time and made many strides in the advancement of technology even before the creation of Nikkes.</li></ul>",
         locations: ["Earth"],
         tags: ["Humanity", "V.T.C."],
-        image: "https://via.placeholder.com/400x200",
+        image: "image/Chapter Thumbnails/Sky2.png",
         youtube: ""
         
     },
