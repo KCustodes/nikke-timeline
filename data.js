ead599871d57d6837d5c5a2796f4818a5f4a29b1
KCustodes <kvplogames@gmail.com>
2026-02-14 13:35:23 -0800

    Update data.js

--- data.js
+++ data.js
@@ -1,87 +1,153 @@
 const ENTRIES = [
-    // ===== MAIN STORY =====
+    // === PRE-CHAPTER 1 (Negative sortOrder = left of Chapter 1) ===
     {
-        id: "ms-001",
+        id: "ms-pre-1",
         type: "main_story",
         title: "Pre-First Invasion",
         era: "Pre-First Invasion",
-        year: "+100 BC1 (Before Chapter 1)",
-        content: "The World was similar to what we know now in real life, but slightly more technologically advanced. <ul><li>Manufactures such as Elysion, Missilis and Tetra existed but no exact detail on how big they were aside from Relics stating them to be enterprises. (Early Relics called them by Elysion Enterprises, Missilis Industries, and Tetra Entertainment).</li><li>Military strength similar to IRL or at least slightly more advanced.</li><li>Einkk stated that she is roughly 20 years old (Memory of Goddess secret cutscene) which puts her origins during Pre-First Invasion but no other specifics on who or what made her.</li><li>Based off Chapter 43, the VTC has existed for a long time and made many strides in the advancement of technology even before the creation of Nikkes.</li></ul>",
+        timeline: "+100 BC1 (Before Chapter 1)",
+        sortOrder: -110,
+        summary: "The World was similar to what we know now in real life, but slightly more technologically advanced. <ul><li>Manufactures such as Elysion, Missilis and Tetra existed but no exact detail on how big they were aside from Relics stating them to be enterprises. (Early Relics called them by Elysion Enterprises, Missilis Industries, and Tetra Entertainment).</li><li>Military strength similar to IRL or at least slightly more advanced.</li><li>Einkk stated that she is roughly 20 years old (Memory of Goddess secret cutscene) which puts her origins during Pre-First Invasion but no other specifics on who or what made her.</li><li>Based off Chapter 43, the VTC has existed for a long time and made many strides in the advancement of technology even before the creation of Nikkes.</li></ul>",
+        content: "Description here",
         locations: ["Earth"],
         tags: ["Humanity", "V.T.C."],
         image: "images/Chapter Thumbnails/Sky2.png",
         youtube: "",
         connections: []
     },
-    
     {
-        id: "ms-002",
+        id: "ms-pre-2",
         type: "main_story",
         title: "The First Invasion",
         era: "The First Invasion",
-        year: "100 BC1 (Before Chapter 1)",
-        content: "Raptures began to invade multiple countries in hordes causing devastation. <ul><li><strong>Theories concluded that Raptures originated from the Space Station.</strong> Theories were reinforced when it was discovered that the Raptures targeted various shuttle-launch locations around the world during the initial attack. <em>(RED ASH INFO)</em></li><li>Initial military response was ill-equipped and resulted in many casualties.</li><li>The use of thermonuclear weapons had little to no effect and some instances had Raptures firing it back at humans. <em>(OLD TALES)</em> <em>(GLUTTONY)</em></li><li>As a result of all these attacks, <strong>The United Forces of Humanity</strong> was formed to be a combined effort against the Rapture threat.</li><li>No information on <strong>VTC</strong> aside from it being a medical institute run by the church but it can be assumed that the same happened like the UFH.</li><li>The first Nikke, <strong>Liliweiss</strong>, was created which followed the 1st and 2nd Generation Grimms models as well as the Mass-Produced models.</li><li>Previously a mercenary, <strong>The Legendary Commander</strong> began to acquire fame and eventually became the commander for the newly formed Goddess Squad.</li><li>Humanity began to win against Raptures as Nikkes proved to be an effective weapon, but due to that Raptures began to evolve.</li></ul>",
-        locations: ["Earth", "Space Station"],
+        timeline: "100 BC1 (Before Chapter 1)",
+        sortOrder: -100,
+        summary: "Raptures began to invade multiple countries in hordes causing devastation. <ul><li><strong>Theories concluded that Raptures originated from the Space Station.</strong> Theories were reinforced when it was discovered that the Raptures targeted various shuttle-launch locations around the world during the initial attack. <em>(RED ASH INFO)</em></li><li>Initial military response was ill-equipped and resulted in many casualties.</li><li>The use of thermonuclear weapons had little to no effect and some instances had Raptures firing it back at humans. <em>(OLD TALES)</em> <em>(GLUTTONY)</em></li><li>As a result of all these attacks, <strong>The United Forces of Humanity</strong> was formed to be a combined effort against the Rapture threat.</li><li>No information on <strong>VTC</strong> aside from it being a medical institute run by the church but it can be assumed that the same happened like the UFH.</li><li>The first Nikke, <strong>Liliweiss</strong>, was created which followed the 1st and 2nd Generation Grimms models as well as the Mass-Produced models.</li><li>Previously a mercenary, <strong>The Legendary Commander</strong> began to acquire fame and eventually became the commander for the newly formed Goddess Squad.</li><li>Humanity began to win against Raptures as Nikkes proved to be an effective weapon, but due to that Raptures began to evolve.</li></ul>",
+        content: "Description Here",
+	locations: ["Earth", "Space Station"],
         tags: ["Humanity", "U.F.H.", "V.T.C."],
         image: "images/Chapter Thumbnails/SpaceElevator.png",
         youtube: "",
         connections: [
-            { target: "ms-001", type: "curved" }
+            { target: "ms-pre-1", type: "curved" }
         ]
-    },    
-    
+    },
+
     {
-        id: "ms-003",
+        id: "ms-pre-3",
         type: "main_story",
         title: "Death of the First Grimms Model",
-        era: "The First Invasion",
-        year: "Unknown Time BC1 (Before Chapter 1)",
-        content: "<ul><li>No specific info on <strong>Grimms Model 1</strong> yet. <em>(Red Ash Relic)</em></li><li><strong>Dorothy</strong> joins Goddess Squad as <strong>Grimms Model 2</strong>. Daughter of a politician, she wants the world to go back to the way it was before the Rapture Invasion.</li><li><strong>Rapunzel</strong> joins Goddess Squad as <strong>Grimms Model 3</strong>. A former leading candidate for the papacy, she wanted to become a beacon of hope for mankind and joined up.</li><li><strong>Snow White</strong> joins Goddess Squad as <strong>Grimms Model 4</strong>. Introduced herself to the squad during one of their battles by mending their weapons. Has met <strong>Liliweiss</strong> before but not stated when exactly. Initially wasn't confident enough to properly meet them and shut herself in her room crafting weapons.</li><li><strong>Red Hood</strong> joins Goddess Squad as <strong>Grimms Model 5</strong>. Making a dramatic entrance by appearing out of a box, Red Hood's introduction to the squad left a significant impression on them.</li></ul>",        locations: ["Earth"],
+        era: "First Invasion",
+        timeline: "Unknown Time BC1 (Before Chapter 1)",
+        sortOrder: -99,
+        summary: "<ul><li>No specific info on <strong>Grimms Model 1</strong> yet. <em>(Red Ash Relic)</em></li><li><strong>Dorothy</strong> joins Goddess Squad as <strong>Grimms Model 2</strong>. Daughter of a politician, she wants the world to go back to the way it was before the Rapture Invasion.</li><li><strong>Rapunzel</strong> joins Goddess Squad as <strong>Grimms Model 3</strong>. A former leading candidate for the papacy, she wanted to become a beacon of hope for mankind and joined up.</li><li><strong>Snow White</strong> joins Goddess Squad as <strong>Grimms Model 4</strong>. Introduced herself to the squad during one of their battles by mending their weapons. Has met <strong>Liliweiss</strong> before but not stated when exactly. Initially wasn't confident enough to properly meet them and shut herself in her room crafting weapons.</li><li><strong>Red Hood</strong> joins Goddess Squad as <strong>Grimms Model 5</strong>. Making a dramatic entrance by appearing out of a box, Red Hood's introduction to the squad left a significant impression on them.</li></ul>",
+        content: "<Description Here",
+        locations: ["Earth"],
         tags: ["Humanity", "U.F.H.", "V.T.C.", "Nikke", "Goddess Squad"],
         image: "",
         youtube: "",
         connections: []
     },
-        
+
     {
-        id: "ms-004",
+        id: "ms-pre-4",
         type: "main_story",
         title: "First Reclamation Campaign",
         era: "Post-Ark Sealing",
-        year: "89? - 80? BC1 (Before Chapter 1)",
-        content: "NO KNOWN INFORMATION AS OF NOW",
+        timeline: "89? - 80? BC1 (Before Chapter 1)",
+        sortOrder: -89,
+        summary: "NO KNOWN INFORMATION AS OF NOW",
+        content: "Description here",
         locations: ["Earth", "The Ark"],
         tags: ["The Ark", "The Central Government", "Nikke", "First Reclamation War"],
         image: "",
         youtube: "",
         connections: []
     },
-        
+
     {
-        id: "ms-005",
+        id: "ms-pre-5",
         type: "main_story",
         title: "Second Reclamation Campaign",
         era: "Post-Ark Sealing",
-        year: "60 BC1 (Before Chapter 1)",
-        content: "",
+        timeline: "60 BC1 (Before Chapter 1)",
+        sortOrder: -60,
+        summary: "NO KNOWN INFORMATION FOR NOW",
+        content: "Description here",
         locations: ["Earth", "The Ark"],
-        tags: ["The Ark", "The Central Government", "Nikke", "First Reclamation War"],
+        tags: ["The Ark", "The Central Government", "Nikke", "Second Reclamation War"],
         image: "",
         youtube: "",
         connections: []
     },
     
-    // ===== EVENT STORY =====
+    // === CHAPTER 1 (Year 0, Starting Point) ===
+    {
+        id: "ms-000",
+        type: "main_story",
+        chapter: 0,
+        title: "Chapter 0: Fall",
+        era: "The Beginning",
+        timeline: "Year 0 (Undetermined Year but possibly 2XXX)",
+        sortOrder: 0,
+        isStartPoint: true,  // This marks it as the starting position
+        summary: "Chapter 1 content...",
+        content: "Full description...",
+        characters: ["The Commander", "Marian", "Rapi", "Anis", "Shifty"],
+        locations: ["Earth"],
+        tags: ["Chapter 1"],
+        image: "",
+        youtube: "",
+        connections: []
+    },
+    
+    // === POST CHAPTER 1 (Positive sortOrder = right of Chapter 1) ===
+    {
+        id: "ms-001",
+        type: "main_story",
+        chapter: 1,
+        title: "Chapter 1: Corruption",
+        era: "The Beginning",
+        timeline: "Within same day as CH 0",
+        sortOrder: 10,
+        summary: "Content...",
+        content: "Full description...",
+        characters: ["The Commander", "Marian", "Rapi", "Anis", "Neon", "Shifty", "Andersen", "Ingrid"],
+        locations: ["Earth", "The Ark"],
+        tags: ["Chapter 2"],
+        image: "",
+        youtube: "",
+        connections: []
+    },
     {
         id: "es-001",
         type: "event_story",
         title: "New Year New Sword",
         era: "The First Invasion",
-        year: "99-96 BC1 (Before Chapter 1)",
-        content: "<ul><li>Set before the events of <strong>Red Ash</strong> and at some point after the <strong>Goddess Squad</strong> is formed.</li><li><strong>Scarlet</strong> and her sister <strong>Rose</strong> are among the few selected for an experimental Nikke squad called <strong>Melee Squad</strong> for infiltration and reclamation.</li><li><strong>Rose</strong> is selected to be the squad leader for having the highest test scores.</li><li>Their first mission resulted in a resounding success with zero casualties.</li><li>As more missions continued to succeed, more Nikkes were lost. Some Nikkes were even prone to <strong>mind switches</strong> due to attacking <strong>Raptures</strong> in head-on confrontations.</li><li>Eventually the missions became more difficult and with each mission came greater casualties. Including both <strong>Rose</strong> and <strong>Scarlet</strong>, the <strong>Melee Squad</strong> is down to six members.</li><li><strong>Rose</strong> begins to question the validity of their circumstances and goes to question the commander. She discovers that the original objective was to slowly kill off the <strong>Melee Squad</strong> until only one was left which then would succeed to becoming a <strong>Grimms Model</strong>.</li><li><strong>Rose</strong> being angered by the deception, kills the commander and another one of their squadmates. She states that she covered herself with a blindfold and imagined them to be <strong>Raptures</strong> in order to prevent a <strong>mind switch</strong> from happening.</li><li>Seeing the destruction that <strong>Rose</strong> had caused, <strong>Scarlet</strong> had no choice but to kill her sister. Stabbing <strong>Rose</strong> through her core, she killed her sister but as well as finding out that <strong>Rose</strong> intended to kill herself off to save <strong>Scarlet</strong>.</li><li>As time passed the <strong>UFH</strong> became aware of what transpired and deemed the project a failure, citing that it was internal conflict that had the Nikkes kill each other.</li><li><strong>Scarlet</strong> took <strong>Rose's</strong> sword, <strong>Fleetly Fading</strong>, since her original sword was taken along with <strong>Rose's</strong> body. Although <strong>Fleetly Fading</strong> was broken, <strong>Scarlet</strong> met a blacksmith that could fix it by using <strong>Rose's Core</strong>.</li></ul>",
+        timeline: "99 - 96 BC1 (Before Chapter 1)",
+        sortOrder: -99,
+        summary: "<ul><li>Set before the events of <strong>Red Ash</strong> and at some point after the <strong>Goddess Squad</strong> is formed.</li><li><strong>Scarlet</strong> and her sister <strong>Rose</strong> are among the few selected for an experimental Nikke squad called <strong>Melee Squad</strong> for infiltration and reclamation.</li><li><strong>Rose</strong> is selected to be the squad leader for having the highest test scores.</li><li>Their first mission resulted in a resounding success with zero casualties.</li><li>As more missions continued to succeed, more Nikkes were lost. Some Nikkes were even prone to <strong>mind switches</strong> due to attacking <strong>Raptures</strong> in head-on confrontations.</li><li>Eventually the missions became more difficult and with each mission came greater casualties. Including both <strong>Rose</strong> and <strong>Scarlet</strong>, the <strong>Melee Squad</strong> is down to six members.</li><li><strong>Rose</strong> begins to question the validity of their circumstances and goes to question the commander. She discovers that the original objective was to slowly kill off the <strong>Melee Squad</strong> until only one was left which then would succeed to becoming a <strong>Grimms Model</strong>.</li><li><strong>Rose</strong> being angered by the deception, kills the commander and another one of their squadmates. She states that she covered herself with a blindfold and imagined them to be <strong>Raptures</strong> in order to prevent a <strong>mind switch</strong> from happening.</li><li>Seeing the destruction that <strong>Rose</strong> had caused, <strong>Scarlet</strong> had no choice but to kill her sister. Stabbing <strong>Rose</strong> through her core, she killed her sister but as well as finding out that <strong>Rose</strong> intended to kill herself off to save <strong>Scarlet</strong>.</li><li>As time passed the <strong>UFH</strong> became aware of what transpired and deemed the project a failure, citing that it was internal conflict that had the Nikkes kill each other.</li><li><strong>Scarlet</strong> took <strong>Rose's</strong> sword, <strong>Fleetly Fading</strong>, since her original sword was taken along with <strong>Rose's</strong> body. Although <strong>Fleetly Fading</strong> was broken, <strong>Scarlet</strong> met a blacksmith that could fix it by using <strong>Rose's Core</strong>.</li></ul>",
+        content: "Full description...",
         characters: ["Scarlet", "Rose"],
         locations: ["Earth"],
         tags: ["New Year New Sword", "Melee Squad", "Scarlet", "Rose"],
+        image: "",
+        youtube: "",
+        connections: []
+    },
+    {
+        id: "ms-002",
+        type: "main_story",
+        chapter: 2,
+        title: "Chapter 2: Identification",
+        era: "The Beginning",
+        timeline: "Within the same day as CH 2",
+        sortOrder: 20,
+        summary: "Content...",
+        content: "Full description...",
+        characters: ["The Commander", "Rapi", "Anis", "Neon", "Shifty", "Andersen", "Ingrid", "Syuen", "Yuni", "Mihara"],
+        locations: ["The Surface", "The Ark", "The Outpost"],
+        tags: ["Chapter 2"],
         image: "",
         youtube: "",
         connections: []
