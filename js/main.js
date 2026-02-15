507d1c0f7f17980d651fafb6666348c6b850d535
KCustodes <kvplogames@gmail.com>
2026-02-14 16:55:14 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// ===== MAIN INITIALIZATION =====
 
+/**
+ * Initialize the application
+ */
+function init() {
+    populateFilters();
+    render();
+    setupEventListeners();
+    checkURLHash();
+    setTimeout(scrollToStartPoint, 300);
+}
+
+// Start the application
+init();
